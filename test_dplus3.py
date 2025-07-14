de8904b08a4a43fc42cd0366bda65071cf102c38
nickhartman24 <156858963+nickhartman24@users.noreply.github.com>
2025-07-14 13:39:45 -0600

    Update test_dplus3.py

--- test_dplus3.py
+++ test_dplus3.py
@@ -430,7 +430,7 @@ while (stop < (last - args.window_size)):
   
 outline = outfile_delim.join([args.chromosome, str(start), str(stop), str(len(positions_list)), str(introgressed_bases), str(introgressed_bases/args.window_size)])
   if args.haplotype:
-    for combo in haplotype_combinations:
+  for combo in haplotype_combinations:
       results[combo]=calculate_introgression_stats(allele_sites[combo],statistics_list)
       #Get introgressed bases per window
       introgressed_bases=np.sum([number_intro_bases(window_start,window_stop,intro_tracts[0],intro_tracts[1]) for intro_tracts in introgressed_tracts[haplotype_combinations[combo][1]]])
